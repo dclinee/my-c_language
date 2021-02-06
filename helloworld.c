c6776a0c8afa41b8468715951e63e58a3c31d8f4
dclinee <dengchanglin8@outlook.com>
2021-02-06 23:00:03 +0800

    Second Commit

--- helloworld.c
+++ helloworld.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	printf("Record the process of learning of C language.");
+	printf("Record the process of learning of C language.\n");
 
 	printf("Hello World! This is a simple program.\n");
 
